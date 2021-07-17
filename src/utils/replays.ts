1614da9421ce14805045eb6f33382f3f5cd44b24
Stone Tao <stonezt2019@gmail.com>
2021-07-17 01:27:43 -0400

    1.2.2 release
    process kaggle team names in replays

--- src/utils/replays.ts
+++ src/utils/replays.ts
@@ -30,7 +30,11 @@ export const parseReplayData = (rawReplayData: any) => {
       ],
       version: rawReplayData.version,
     };
-    console.log('PARSED REPLAY', replay);
+    replay.teamDetails[0].name = rawReplayData.info.TeamNames[0];
+    replay.teamDetails[1].name = rawReplayData.info.TeamNames[1];
+
+    console.log('Parsed Kaggle Replay meta', replay);
+    console.log(`Read ${rawReplayData.steps.length} steps`);
     return replay;
   } else {
     return rawReplayData;
